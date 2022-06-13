2e164a09ff08e26ae55610083ef010180d230c92
Theron Packus <tlpackus@gmail.com>
2022-06-13 16:44:14 -0700

    update to copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Crystal from './Crystal.png';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gold'>
+        <header className="App-header">
+        <img src={Crystal} className="App-logo" alt="logo" />
+        <h1 className='App-title'>Checkout Links Demo #1</h1>
+        <CopyToClipboard text='https://www.youtube.com/watch?v=DiSvq5SgLMI'>
+          <button>Youtube Link Test</button>
+        </CopyToClipboard>
+        </header>
+      </div>
     </div>
   );
 }
